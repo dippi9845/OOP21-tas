1e100dff8a12320e2d0e037305f8534acae94239
Norby99 <72309039+Norby99@users.noreply.github.com>
2022-03-08 16:42:49 +0100

    added first main class

---
+++ src/main/java/TowersAndStuff.java
@@ -0,0 +1,19 @@
+package main.java;
+
+/**
+ * The main class that launch the game
+ *
+ */
+public final class TowersAndStuff {
+
+	private TowersAndStuff() { }
+	
+	/**
+     *
+     * @param args not used
+     */
+	public static void main(final String[] args) {
+        new GameController //TODO: create controller...
+    }
+	
+}
