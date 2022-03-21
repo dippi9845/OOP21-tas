ba0f84e87c71389f0e23362392cda10f987fce31
Norby99 <72309039+Norby99@users.noreply.github.com>
2022-03-21 16:43:48 +0100

    implemented enemies handler

---
+++ src/main/java/model/EnemiesHandler.java
@@ -0,0 +1,7 @@
+package main.java.model;
+
+public interface EnemiesHandler {
+    
+    int getWave();
+    
+}
---
+++ src/main/java/model/EnemiesHandlerImpl.java
@@ -0,0 +1,38 @@
+package main.java.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EnemiesHandlerImpl implements EnemiesHandler {
+    
+    private final Position enemySpawner;
+    private List<Enemy> enemiesList = new ArrayList<Enemy>();
+    private int actualWave;
+    
+    public EnemiesHandlerImpl(Position enemySpawner) {
+        this.enemySpawner = enemySpawner;
+        this.actualWave = 0;
+        
+        setNextWave();
+    }
+
+    private void setNextWave() {
+        this.actualWave++;
+        spawnTestEnemy();
+        //TODO: manca l'implementazione dei nemici che spawnano
+        
+    }
+    
+    private void spawnTestEnemy() {
+        //TODO: questo metodo serve solo per testare il giusto spawn dei nemici. Da cancellare!!!
+        this.enemiesList.add(new RedEnemy(enemySpawner));
+        System.out.println("Test Enemy spawned");
+        
+    }
+
+    @Override
+    public int getWave() {
+        return this.actualWave;
+    }
+
+}
