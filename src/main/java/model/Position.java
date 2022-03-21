6cddc74c5ea32e60fa2821c1548ffe9612aade43
Norby99 <72309039+Norby99@users.noreply.github.com>
2022-03-21 16:43:59 +0100

    Create Position.java

---
+++ src/main/java/model/Position.java
@@ -0,0 +1,21 @@
+package main.java.model;
+
+public class Position {
+
+    private int X;
+    private int Y;
+    
+    public Position(int X, int Y) {
+        this.X = X;
+        this.Y = Y;
+    }
+    
+    public int getX() {
+        return this.X;
+    }
+    
+    public int getY() {
+        return this.Y;
+    }
+    
+}
