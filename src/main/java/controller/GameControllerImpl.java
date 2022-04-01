606e317dd63cb4b83bf9de426a5f22ef6dd73c30
Norby99 <72309039+Norby99@users.noreply.github.com>
2022-04-01 20:03:07 +0200

    created first setup

--- src/main/java/controller/GameControllerImpl.java
+++ src/main/java/controller/GameControllerImpl.java
@@ -1,25 +1,37 @@
 package main.java.controller;
 
+import main.java.model.EnemiesHandler;
 import main.java.model.EnemiesHandlerImpl;
-import main.java.model.Position;
-import main.java.view.GameSceneImpl;
+import main.java.model.enemies.Enemy;
+import main.java.utils.Position;
+import main.java.view.GameScene;
 
 public class GameControllerImpl extends GameController {
     
-    final GameSceneImpl view;
-    private final EnemiesHandlerImpl enemiesHandler;
+    final GameScene gameScene;
+    private final EnemiesHandler enemiesHandler;
     
-    public GameControllerImpl(final GameSceneImpl scene) {
-        this.view = scene;
+    public GameControllerImpl(final GameScene scene) {
+        this.gameScene = scene;
         
-        this.enemiesHandler = new EnemiesHandlerImpl(new Position(100, 100));
-        //TODO: manca l'inserimento della posizione dello spawner e altro...
+        this.enemiesHandler = new EnemiesHandlerImpl(new Position(500, 500));
+        //TODO: manca l'inserimento dinamico della posizione dello spawner e altro...
         
     }
-
+    
+    private void spawnEnemy() {
+        Enemy enemy = this.enemiesHandler.spawnTestEnemy();
+        this.gameScene.getGameView().addEntityLabel(enemy);
+    }
+    
     @Override
     public void nextTick() {
+        if (this.enemiesHandler.cleanWave()) {  //TODO: questa condizione verra cambiata
+            spawnEnemy();
+        }
+        
         this.enemiesHandler.moveEnemies();
+        this.gameScene.getGameView().redrawEntities();
     }
     
 }
