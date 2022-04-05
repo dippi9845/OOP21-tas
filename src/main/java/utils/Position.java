4598f7e63f244740798a8686b57417541ab83483
Norby99 <72309039+Norby99@users.noreply.github.com>
2022-04-05 14:45:24 +0200

    implemented set position and findDistance

--- src/main/java/utils/Position.java
+++ src/main/java/utils/Position.java
@@ -9,6 +9,10 @@ public class Position {
         setPosition(X, Y);
     }
     
+    public void setPosition(Position pos) {
+        setPosition(pos.getX(), pos.getY());
+    }
+    
     public void setPosition(double X, double Y) {
         this.X = X;
         this.Y = Y;
@@ -20,6 +24,16 @@ public class Position {
     
     public double getY() {
         return this.Y;
+    }
+    
+    public static double findDistance(Position pos1, Position pos2) {
+        return Math.hypot(pos1.getX()-pos2.getX(), pos1.getY()-pos2.getY());
+    }
+    
+    
+    @Override
+    public String toString() {
+        return new String(X + " - " + Y);
     }
     
 }
