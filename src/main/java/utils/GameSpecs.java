db7e05b41155abad8af8cb88902717fc574fdea3
Norby99 <72309039+Norby99@users.noreply.github.com>
2022-04-06 12:22:38 +0200

    increased tick rate

--- src/main/java/utils/GameSpecs.java
+++ src/main/java/utils/GameSpecs.java
@@ -7,7 +7,7 @@ public final class GameSpecs {
     private final static int MAX_UNIT = 1000;
     public final static Dimension GAME_UNITS = new Dimension(MAX_UNIT, MAX_UNIT);
     
-    private final static int TICKS_PER_SECOND = 30;
+    public final static int TICKS_PER_SECOND = 60;
     public final static int SKIP_TICKS = 1000 / TICKS_PER_SECOND;
     public final static int MAX_FRAMESKIP = 5;
     
