55d182e4a4a1250372c4df602aa547817d06d70c
Norby99 <72309039+Norby99@users.noreply.github.com>
2022-04-07 18:24:26 +0200

    created test class for GameModelImpl

---
+++ test/main/java/tas/model/GameModelImplTest.java
@@ -0,0 +1,39 @@
+package main.java.tas.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GameModelImplTest {
+
+    @Test
+    public void testGiveMoney2Player() {
+        final int startingHP = 100;
+        final int startingMoney = 100;
+        final GameModel model = new GameModelImpl(startingHP, startingMoney);
+        final int moneyToBeGiven = 7;
+        final int numberOfTransitions = 5;
+        
+        for (int i=0; i < numberOfTransitions; i++) {
+            model.giveMoney2Player(moneyToBeGiven);
+        }
+        
+        assertEquals(model.getPlayerMoney(), startingMoney + (moneyToBeGiven * numberOfTransitions));
+    }
+
+    @Test
+    public void testDealDamage2Player() {
+        final int startingHP = 100;
+        final int startingMoney = 100;
+        final GameModel model = new GameModelImpl(startingHP, startingMoney);
+        final int attackDamage = 10;
+        final int numberOfAttacks = 3;
+        
+        for (int i=0; i < numberOfAttacks; i++) {
+            model.dealDamage2Player(attackDamage);
+        }
+        
+        assertEquals(model.getHP(), startingHP - (attackDamage * numberOfAttacks));
+    }
+
+}
