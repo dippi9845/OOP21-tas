19777b9eb91205d6041cbd1e25b24c85334c4925
Filippo Di Pietro <filippo.dipietro3@studio.unibo.it>
2022-04-13 20:43:47 +0200

    Added global list of enemys

--- src/main/java/tas/tower/Towers.java
+++ src/main/java/tas/tower/Towers.java
@@ -1,11 +1,13 @@
 package main.java.tas.tower;
 
 import java.util.List;
-
+import java.util.LinkedList;
 import main.java.tas.model.enemies.Enemy;
 import main.java.tas.utils.Position;
 
 public class Towers {
+	static public final List<Enemy> ENEMYLIST = new LinkedList<Enemy>();
+	
 	static public boolean isValidTarget(final Enemy e, final Tower t) {
 		if (Position.findDistance(t.getPos(), e.getPosition()) <= t.getRadius()) {
 			return true;
@@ -15,9 +17,9 @@ public class Towers {
 		}
 	}
 	
-	static public Enemy findFirstEnemy(final List<Enemy> enemyList, final Tower t) {
+	static public Enemy findFirstEnemy(final Tower t) {
 		Enemy e = null;
-		for (var i : enemyList) {
+		for (var i : ENEMYLIST) {
 			if (Towers.isValidTarget(i, t)) {
 				e = i;
 				break; // Brutto ma necessario
