60e0e0dbf325ad49e92dee2a87355aeed441d7b9
Norby99 <72309039+Norby99@users.noreply.github.com>
2022-04-13 11:45:59 +0200

    refactory
    converted enemiesHandler to enemyFactory and enemiesLogic

--- src/main/java/tas/model/EnemiesLogic.java
+++ src/main/java/tas/model/EnemiesLogic.java
@@ -4,9 +4,8 @@ import java.util.List;
 
 import main.java.tas.model.enemies.Enemy;
 
-public interface EnemiesHandler {
-    
-    
+public interface EnemiesLogic {
+
     Enemy spawnEnemy();
     void removeEnemy(Enemy enemy);
     
@@ -14,5 +13,5 @@ public interface EnemiesHandler {
     int getWave();
     void setNextWave();
     List<Enemy> getEnemies();
- 
+    
 }
---
+++ src/main/java/tas/model/EnemiesLogicImpl.java
@@ -0,0 +1,53 @@
+package main.java.tas.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import main.java.tas.model.enemies.Enemy;
+import main.java.tas.utils.Position;
+
+public class EnemiesLogicImpl implements EnemiesLogic {
+    
+    private List<Enemy> aliveEnemiesList = new ArrayList<Enemy>();
+    private int actualWave;
+    private final EnemyFactory enemyFactory;
+
+    public EnemiesLogicImpl(List<Position> nodesPosition) {
+        this.enemyFactory = new EnemyFactoryImpl(nodesPosition);
+        this.actualWave = 0;
+    }
+
+    @Override
+    public Enemy spawnEnemy() {
+        Enemy enemy = this.enemyFactory.spawnRedEnemy();
+        this.aliveEnemiesList.add(enemy);
+        
+        return enemy;
+    }
+
+    @Override
+    public void removeEnemy(Enemy enemy) {
+        this.aliveEnemiesList.remove(enemy);
+    }
+
+    @Override
+    public boolean isWaveClean() {
+        return aliveEnemiesList.isEmpty();
+    }
+
+    @Override
+    public int getWave() {
+        return this.actualWave;
+    }
+
+    @Override
+    public void setNextWave() {
+        this.actualWave++;
+    }
+
+    @Override
+    public List<Enemy> getEnemies() {
+        return aliveEnemiesList;
+    }
+    
+}
