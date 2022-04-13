00348302f25a7924493847314e60188863adf66f
Filippo Di Pietro <filippodp9845@gmail.com>
2022-04-13 17:52:34 +0200

    Renamed class Tower and removed isValidTarget

--- src/main/java/tas/tower/AbstractBasicTower.java
+++ src/main/java/tas/tower/AbstractBasicTower.java
@@ -2,30 +2,33 @@ package main.java.tas.tower;
 import main.java.tas.model.enemies.Enemy;
 import main.java.tas.utils.Position;
 
-public abstract class Tower {
+public abstract class AbstractBasicTower implements Tower{
 	private Enemy target;
 	private final int damage;
 	private final Position pos;
 	private final int radius;
 	
-	Tower(final Position pos, final int damage, final int radius) {
+	AbstractBasicTower(final Position pos, final int damage, final int radius) {
 		this.damage = damage;
 		this.pos = pos;
 		this.radius = radius;
 	}
 	
 	abstract protected void attack();
-	
-	protected boolean isValidTarget(final Enemy e) {
-		if (Position.findDistance(this.pos, e.getPosition()) <= this.radius) {
-			return true;
-		}
-		else {
-			return false;
-		}
-	}
 
 	abstract protected void setTarget(Enemy e);
 	
 	abstract public void compute();
+	
+	public int getDamage() {
+		return this.damage;
+	}
+	
+	public Position getPos() {
+		return this.pos;
+	}
+	
+	public int getRadius() {
+		return this.radius;
+	}
 }
