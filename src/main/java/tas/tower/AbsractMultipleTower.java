a171927a0340827260fd75d1cf31d992b44e7702
Filippo Di Pietro <filippo.dipietro3@studio.unibo.it>
2022-04-15 01:53:01 +0200

    Added new type of tower

---
+++ src/main/java/tas/tower/AbsractMultipleTower.java
@@ -0,0 +1,34 @@
+package main.java.tas.tower;
+
+import java.util.LinkedList;
+import java.util.List;
+import main.java.tas.model.enemies.Enemy;
+import main.java.tas.utils.Position;
+
+public abstract class AbsractMultipleTower extends AbstractBasicTower implements Tower {
+	final List<Enemy> enemyList = new LinkedList<>();
+	final int maxEnemy;
+	
+	
+	protected AbsractMultipleTower(Position pos, int damage, int radius, int delay, final int maxTarget) {
+		super(pos, damage, radius, delay);
+		this.maxEnemy = maxTarget;
+	}
+
+	@Override
+	protected void attack() {
+		
+	}
+
+	@Override
+	protected void setTarget(Enemy e) {
+		
+	}
+
+	@Override
+	public void compute() {
+		
+
+	}
+
+}
