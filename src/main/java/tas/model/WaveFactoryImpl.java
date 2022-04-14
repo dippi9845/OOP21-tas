66128fa344c95248b841fb7ceebb444b940eb0d1
Norby99 <72309039+Norby99@users.noreply.github.com>
2022-04-14 19:18:55 +0200

    implemented dynamic spawn of enemies

--- src/main/java/tas/model/WaveFactoryImpl.java
+++ src/main/java/tas/model/WaveFactoryImpl.java
@@ -16,10 +16,47 @@ public class WaveFactoryImpl implements WaveFactory {
     @Override
     public List<Enemy> createEnemiesToBeSpawn(int wave) {
         List<Enemy> enemiesList = new ArrayList<Enemy>();
-        for (int i=0; i<wave*2; i++) {
-            enemiesList.add(this.enemyFactory.spawnRedEnemy());
-        }
+        
+        enemiesList.addAll(getRedEnemyByWave(wave));
+        enemiesList.addAll(getPinkEnemyByWave(wave));
+        
         return enemiesList;
+    }
+    
+    private List<Enemy> getRedEnemyByWave(int wave) {
+        int enemiesForWave = 0;
+        List<Enemy> eList = new ArrayList<Enemy>();
+        
+        if (0 < wave && wave <= 3) {
+            enemiesForWave = (int) Math.round(0.655 * wave + 5);
+        }
+        if (3 < wave && wave < 10) {
+            enemiesForWave = (int) Math.round(-(0.095*Math.pow((wave-5.2), 4) - 0.2*Math.pow((wave-5.2), 3) - 1.2*Math.pow((wave-5.2), 2) + 0.8*(wave-5.2)) + 3.7);
+        }
+        
+        for (int i=0; i < enemiesForWave; i++) {
+            eList.add(this.enemyFactory.spawnRedEnemy());
+        }
+        
+        return eList;
+    }
+    
+    private List<Enemy> getPinkEnemyByWave(int wave) {
+        int enemiesForWave = 0;
+        List<Enemy> eList = new ArrayList<Enemy>();
+        
+        if (5.4 < wave && wave < 12) {
+            enemiesForWave = (int) Math.round(0.57*Math.pow((wave-10), 3) + 2.6*Math.pow((wave-10), 2));
+        }
+        if (wave >= 12) {
+            enemiesForWave = (int) Math.round(0.5*wave + 9);
+        }
+        
+        for (int i=0; i < enemiesForWave; i++) {
+            eList.add(this.enemyFactory.spawnPinkEnemy());
+        }
+        
+        return eList;
     }
 
 }
