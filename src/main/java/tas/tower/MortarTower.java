59d91db894023c87bb8bef179b9990d561ac7cee
Filippo Di Pietro <filippo.dipietro3@studio.unibo.it>
2022-04-19 15:05:48 +0200

    Added fields to mortar

--- src/main/java/tas/tower/MortarTower.java
+++ src/main/java/tas/tower/MortarTower.java
@@ -6,11 +6,14 @@ import main.java.tas.model.enemies.Enemy;
 import main.java.tas.utils.Position;
 
 public class MortarTower extends AbstractMultipleTower {
-	
+	private final int minRange;
+	private final int attackRange;
+	private Position center;
 	
 	protected MortarTower(Position pos, int damage, int radius, int delay, int maxTarget, int minRange, int attackRange) {
 		super(pos, damage, radius, delay, Integer.MAX_VALUE);
-
+		this.minRange = minRange;
+		this.attackRange = attackRange;
 	}
 	
 	private boolean isFirstInRange(final Enemy e) {
