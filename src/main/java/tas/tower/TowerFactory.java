19a71221f2b5cc20be172606e01043a67fc01561
Filippo Di Pietro <filippo.dipietro3@studio.unibo.it>
2022-05-11 13:58:40 +0200

    Added TowerFactory interface

---
+++ src/main/java/tas/tower/TowerFactory.java
@@ -0,0 +1,15 @@
+package main.java.tas.tower;
+
+public interface TowerFactory {
+	Tower cannon();
+	
+	Tower archerTower();
+	
+	Tower flamethrower();
+	
+	Tower gastower();
+	
+	Tower mortar();
+	
+	Tower tesla();
+}
