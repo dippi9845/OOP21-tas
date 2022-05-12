7fd0a3a1a7ea25ec74286db73890d7e242357800
Filippo Di Pietro <filippo.dipietro3@studio.unibo.it>
2022-05-12 20:58:37 +0200

    first implementation of tower control

---
+++ src/main/java/tas/controller/TowerLogic.java
@@ -0,0 +1,9 @@
+package main.java.tas.controller;
+
+import main.java.tas.model.tower.Tower;
+
+public interface TowerLogic {
+	
+	// add tower t to the list of current towers
+	void buildTower(final Tower t);
+}
