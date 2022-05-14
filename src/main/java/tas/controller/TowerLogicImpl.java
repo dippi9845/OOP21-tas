2f2c141dc13c068b462f8f14f7892dacd904dc46
Filippo Di Pietro <filippo.dipietro3@studio.unibo.it>
2022-05-14 02:03:36 +0200

    Added field consumer that draws the towers, when buildTower is called
    in order to abstract from other implementations

--- src/main/java/tas/controller/TowerLogicImpl.java
+++ src/main/java/tas/controller/TowerLogicImpl.java
@@ -1,7 +1,9 @@
 package main.java.tas.controller;
 
 import java.util.List;
+import java.util.function.Consumer;
 
+import main.java.tas.model.Entity;
 import main.java.tas.model.enemies.Enemy;
 import main.java.tas.model.tower.Tower;
 import main.java.tas.model.tower.Towers;
@@ -9,15 +11,18 @@ import java.util.LinkedList;
 
 public class TowerLogicImpl implements TowerLogic {
 	private final List<Thread> builtTowers = new LinkedList<Thread>();
-
-	public TowerLogicImpl(final List<Enemy> enemyList) {
+	private final Consumer<Entity> addToPanel;
+	
+	public TowerLogicImpl(final List<Enemy> enemyList, final Consumer<Entity> addToPanel) {
 		Towers.ENEMYLIST = enemyList;
+		this.addToPanel = addToPanel;
 	}
 	
 	@Override
 	public void buildTower(final Tower t) {
 		this.builtTowers.add(new Thread(t));
 		this.builtTowers.get(this.builtTowers.size() - 1).run();
+		this.addToPanel.accept(t);
 	}
 	
 	// TODO abbelire
