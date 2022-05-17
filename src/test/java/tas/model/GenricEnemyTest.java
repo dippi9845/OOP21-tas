2fe7cb768df5874375e9bf41f631790d878be344
Norby99 <72309039+Norby99@users.noreply.github.com>
2022-05-17 13:49:39 +0200

    implemented GenericEnemy in tests

--- src/test/java/tas/model/GenricEnemyTest.java
+++ src/test/java/tas/model/GenricEnemyTest.java
@@ -2,12 +2,13 @@ package test.java.tas.model;
 
 import static org.junit.Assert.*;
 
+import java.awt.Dimension;
 import java.util.Arrays;
 
 import org.junit.Test;
 
 import main.java.tas.model.enemies.Enemy;
-import main.java.tas.model.enemies.RedEnemy;
+import main.java.tas.model.enemies.GenericEnemy;
 import main.java.tas.utils.Position;
 
 public class GenricEnemyTest {
@@ -15,7 +16,7 @@ public class GenricEnemyTest {
     @Test
     public void testGetPosition() {
         final Position pos = new Position(10, 10);
-        final Enemy enemy = new RedEnemy(Arrays.asList(pos));
+        final Enemy enemy = new GenericEnemy(Arrays.asList(pos), 1, 50, 10, 60, new Dimension(100, 100), "redEnemy");
         
         assertEquals(pos.toString(), enemy.getPosition().toString());
     }
@@ -25,8 +26,8 @@ public class GenricEnemyTest {
         final Position pos1 = new Position(10, 10);
         final Position pos2 = new Position(20, 20);
         final Position pos3 = new Position(10, 10);
-        final Enemy enemy1 = new RedEnemy(Arrays.asList(pos1, pos2));
-        final Enemy enemy2 = new RedEnemy(Arrays.asList(pos1, pos3));
+        final Enemy enemy1 = new GenericEnemy(Arrays.asList(pos1, pos2), 1, 50, 10, 60, new Dimension(100, 100), "redEnemy");
+        final Enemy enemy2 = new GenericEnemy(Arrays.asList(pos1, pos3), 1, 50, 10, 60, new Dimension(100, 100), "redEnemy");
         
         enemy1.moveForward();
         enemy2.moveForward();
@@ -38,7 +39,7 @@ public class GenricEnemyTest {
     @Test
     public void testDealDamage() {
         final Position pos = new Position(10, 10);
-        final Enemy enemy = new RedEnemy(Arrays.asList(pos));
+        final Enemy enemy = new GenericEnemy(Arrays.asList(pos), 1, 50, 10, 60, new Dimension(100, 100), "redEnemy");
         final double maxHealth = enemy.getHealth();
         final double damage = 5;
         
@@ -50,7 +51,7 @@ public class GenricEnemyTest {
     @Test
     public void testIsDead() {
         final Position pos = new Position(10, 10);
-        final Enemy enemy = new RedEnemy(Arrays.asList(pos));
+        final Enemy enemy = new GenericEnemy(Arrays.asList(pos), 1, 50, 10, 60, new Dimension(100, 100), "redEnemy");
         
         assertFalse(enemy.isDead());
         
@@ -63,8 +64,8 @@ public class GenricEnemyTest {
     public void testHasCompletedPath() {
         final Position pos1 = new Position(10, 10);
         final Position pos2 = new Position(20, 20);
-        final Enemy enemy1 = new RedEnemy(Arrays.asList(pos1));
-        final Enemy enemy2 = new RedEnemy(Arrays.asList(pos1, pos2));
+        final Enemy enemy1 = new GenericEnemy(Arrays.asList(pos1), 1, 50, 10, 60, new Dimension(100, 100), "redEnemy");
+        final Enemy enemy2 = new GenericEnemy(Arrays.asList(pos1, pos2), 1, 50, 10, 60, new Dimension(100, 100), "redEnemy");
         
         assertFalse(enemy2.hasCompletedPath());
         assertTrue(enemy1.hasCompletedPath());
@@ -72,7 +73,7 @@ public class GenricEnemyTest {
     
     @Test(expected = IllegalArgumentException.class)
     public void testCreationNoNode() {
-        new RedEnemy(Arrays.asList());
+        new GenericEnemy(Arrays.asList(), 1, 50, 10, 60, new Dimension(100, 100), "redEnemy");
     }
 
 }
