41401f820dba09ef92fc8cc5f5ac55c21791cf8a
Norby99 <72309039+Norby99@users.noreply.github.com>
2022-05-19 17:50:52 +0200

    implemented test class

---
+++ src/test/java/tas/model/EnemyFactoryImplTest.java
@@ -0,0 +1,22 @@
+package test.java.tas.model;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+import main.java.tas.model.enemies.EnemyFactory;
+import main.java.tas.model.enemies.EnemyFactoryImpl;
+import main.java.tas.utils.Position;
+
+public class EnemyFactoryImplTest {
+
+    @Test
+    public void testCreateEnemiesToBeSpawn() {
+        EnemyFactory enemyFactory = new EnemyFactoryImpl(Arrays.asList(new Position(0, 0)));
+        
+        assertFalse(enemyFactory.createEnemiesToBeSpawn(1).isEmpty());
+    }
+
+}
