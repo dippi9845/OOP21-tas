3c5358add93a9f67457b1cd3fc5063dc070f7de7
Filippo Di Pietro <filippo.dipietro3@studio.unibo.it>
2022-05-24 15:40:27 +0200

    added test class for towers

---
+++ src/test/java/tas/model/tower/AttackTest.java
@@ -0,0 +1,14 @@
+package test.java.tas.model.tower;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AttackTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
