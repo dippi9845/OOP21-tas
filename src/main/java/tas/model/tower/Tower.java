6aeb156e2d78e439fbf12da268a407216f915e86
Filippo Di Pietro <filippo.dipietro3@studio.unibo.it>
2022-05-28 03:34:47 +0200

    Added javadoc to Tower.java

--- src/main/java/tas/model/tower/Tower.java
+++ src/main/java/tas/model/tower/Tower.java
@@ -6,21 +6,33 @@ import java.awt.Dimension;
 
 public interface Tower extends Entity, Runnable {
 	
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	default Dimension getBodyDimension() {
 		return new Dimension(100, 100);
 	}
 	
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	default Position getPosition() {
 		return this.getPos();
 	}
 	
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	default String getImageName() {
 		return this.getTowerImageName();
 	}
 	
+	/**
+	 * Method used by thread, to run the tower in concurrency
+	 */
 	default void run() {
 		try {
 			this.compute();
@@ -29,17 +41,39 @@ public interface Tower extends Entity, Runnable {
 		}
 	}
 	
+	/**
+	 * This method implements the behavior of the tower, at every wake up of the Thread
+	 * @throws InterruptedException
+	 */
 	public void compute() throws InterruptedException;
 	
+	/**
+	 * @return Returns the damage of the tower
+	 */
 	public int getDamage();
 	
+	/**
+	 * @return Returns the radius of the tower
+	 */
 	public int getRadius();
 	
+	/**
+	 * @return Returns the cost of the tower
+	 */
 	public int getCost();
 	
+	/**
+	 * @return Returns the delay of the tower
+	 */
 	public int getDelay();
 	
+	/**
+	 * @return Returns the name of the image linked to the tower
+	 */
 	public String getTowerImageName();
-
+	
+	/**
+	 * @return Returns the position of the tower
+	 */
 	public Position getPos();
 }
