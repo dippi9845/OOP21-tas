4269225939a7a952ea9170491130a25b6ad08baa
Filippo Di Pietro <filippo.dipietro3@studio.unibo.it>
2022-05-28 04:09:57 +0200

    Added javadoc comments to AbstractMultipleTower

--- src/main/java/tas/model/tower/AbstractMultipleTower.java
+++ src/main/java/tas/model/tower/AbstractMultipleTower.java
@@ -7,26 +7,50 @@ import main.java.tas.model.enemies.Enemy;
 import main.java.tas.utils.Position;
 
 public abstract class AbstractMultipleTower extends AbstractBasicTower implements Tower {
-	private final List<Enemy> enemyList = new LinkedList<>(); // TODO forse meglio un arraylist
+	private final List<Enemy> enemyList = new LinkedList<>();
 	private final int maxEnemy;
-	
+	/**
+	 * Constructor, protected
+	 * @param pos Tower position
+	 * @param damage Tower damage
+	 * @param radius Tower radius, where it can attack enemies
+	 * @param delay Tower delay
+	 * @param cost Tower cost
+	 * @param imageName  Tower image name
+	 * @param maxTarget Max number of target that this tower can handle at the time
+	 */
 	protected AbstractMultipleTower(final Position pos, final int damage, final int radius, final int delay, final int cost, final String imageName, final int maxTarget) {
 		super(pos, damage, radius, delay, cost, imageName);
 		this.maxEnemy = maxTarget;
 	}
-
+	
+	/**
+	 * Check if an enemy is contained
+	 * @param e Enemy to check if is contained
+	 * @return true if the enemy is contained, false otherwise
+	 */
 	protected boolean contains(final Enemy e) {
 		return this.enemyList.contains(e);
 	}
 	
+	/**
+	 * @return an immutable list of current enemies targeted
+	 */
 	protected List<Enemy> getEnemyList() {
 		return Collections.unmodifiableList(this.enemyList);
 	}
-	
+
+	/**
+	 * @return the maximum enemy to be targeted by the tower
+	 */
 	protected int getMaxEnemy() {
 		return this.maxEnemy;
 	}
-	
+
+	/**
+	 * Check if the list is full
+	 * @return true if the
+	 */
 	protected boolean isFull() {
 		return this.enemyList.size() == this.maxEnemy;
 	}
