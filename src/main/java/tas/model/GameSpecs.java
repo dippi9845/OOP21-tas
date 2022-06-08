480456f05520ae0ade5b9e21d94a2bb4a98110f1
Norby99 <72309039+Norby99@users.noreply.github.com>
2022-06-08 18:18:21 +0200

    adapted class to get data from json file

--- src/main/java/tas/model/GameSpecs.java
+++ src/main/java/tas/model/GameSpecs.java
@@ -2,16 +2,49 @@ package main.java.tas.model;
 
 import java.awt.Dimension;
 
+import org.json.JSONObject;
+
+import main.java.tas.utils.JsonUtils;
+
 /**
  * Class that handles some game attributes.
  */
 public final class GameSpecs {
 
-    private final static int MAX_UNIT = 1000;
-    public final static Dimension GAME_UNITS = new Dimension(MAX_UNIT, MAX_UNIT);
+    private static String RESOURCE_PATH = "res";
+    private final String enemiesJsonSetup = RESOURCE_PATH + System.getProperty("file.separator") + "data"
+            + System.getProperty("file.separator") + "game" + System.getProperty("file.separator") + "gameSpecs.json";
 
-    public final static int TICKS_PER_SECOND = 60;
-    public final static int SKIP_TICKS = 1000 / TICKS_PER_SECOND;
-    public final static int MAX_FRAMESKIP = 5;
+    private final Dimension gameUnits;
+
+    private final int tickRate;
+    private final int skipTicks;
+    private final int maxFrameSkip;
+
+    public GameSpecs() {
+        JSONObject jsonSetupList = JsonUtils.getJsonData(enemiesJsonSetup);
+
+        this.gameUnits = new Dimension(jsonSetupList.getJSONObject("gameUnits").getInt("width"),
+                jsonSetupList.getJSONObject("gameUnits").getInt("height"));
+        this.tickRate = jsonSetupList.getInt("tickRate");
+        this.maxFrameSkip = jsonSetupList.getInt("maxFrameSkip");
+        this.skipTicks = 1000 / this.tickRate;
+    }
+
+    public Dimension getGameUnits() {
+        return this.gameUnits;
+    }
+
+    public int getTickRate() {
+        return this.tickRate;
+    }
+
+    public int getSkipTicks() {
+        return this.skipTicks;
+    }
+
+    public int getMaxFrameSkip() {
+        return this.maxFrameSkip;
+    }
 
 }
