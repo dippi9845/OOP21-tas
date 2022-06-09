df51141453588008d916f70309aa9ef0eaa40d02
Filippo Di Pietro <filippo.dipietro3@studio.unibo.it>
2022-06-10 01:04:47 +0200

    Formatted better

--- src/main/java/tas/controller/EnemiesLogicImpl.java
+++ src/main/java/tas/controller/EnemiesLogicImpl.java
@@ -15,81 +15,81 @@ import main.java.tas.utils.Position;
  */
 public class EnemiesLogicImpl implements EnemiesLogic {
 
-    private final List<Enemy> aliveEnemiesList = new ArrayList<Enemy>();
-    private int actualWave;
-    private final EnemyFactory waveFactory;
-    private List<Enemy> enemyToBeSpawned = new ArrayList<Enemy>();
+	private final List<Enemy> aliveEnemiesList = new ArrayList<Enemy>();
+	private int actualWave;
+	private final EnemyFactory waveFactory;
+	private List<Enemy> enemyToBeSpawned = new ArrayList<Enemy>();
 
-    /**
-     * Constructor that creates the logic of the enemy waves.
-     * 
-     * @param nodesPosition is a list with the nodes that the enemies will have to
-     *                      travel
-     */
-    public EnemiesLogicImpl(List<Position> nodesPosition) {
-        this.waveFactory = new EnemyFactoryImpl(nodesPosition);
-        this.actualWave = 0;
-    }
+	/**
+	 * Constructor that creates the logic of the enemy waves.
+	 * 
+	 * @param nodesPosition is a list with the nodes that the enemies will have to
+	 *                      travel
+	 */
+	public EnemiesLogicImpl(List<Position> nodesPosition) {
+		this.waveFactory = new EnemyFactoryImpl(nodesPosition);
+		this.actualWave = 0;
+	}
 
-    /** {@inheritDoc} */
+	/** {@inheritDoc} */
 
-    // TODO: return optional
-    @Override
-    public Optional<Enemy> spawnEnemy() {
-        if (this.enemyToBeSpawned.isEmpty()) {
-            return Optional.empty();
-        }
+	// TODO: return optional
+	@Override
+	public Optional<Enemy> spawnEnemy() {
+		if (this.enemyToBeSpawned.isEmpty()) {
+			return Optional.empty();
+		}
 
-        Enemy enemy = this.enemyToBeSpawned.remove(0);
-        this.aliveEnemiesList.add(enemy);
+		Enemy enemy = this.enemyToBeSpawned.remove(0);
+		this.aliveEnemiesList.add(enemy);
 
-        return Optional.of(enemy);
-    }
+		return Optional.of(enemy);
+	}
 
-    /** {@inheritDoc} */
-    @Override
-    public void removeEnemy(Enemy enemy) throws NoSuchFieldException {
-        if (!this.aliveEnemiesList.contains(enemy)) {
-            throw new NoSuchFieldException("This enemy is not alive");
-        }
-        this.aliveEnemiesList.remove(enemy);
-    }
+	/** {@inheritDoc} */
+	@Override
+	public void removeEnemy(Enemy enemy) throws NoSuchFieldException {
+		if (!this.aliveEnemiesList.contains(enemy)) {
+			throw new NoSuchFieldException("This enemy is not alive");
+		}
+		this.aliveEnemiesList.remove(enemy);
+	}
 
-    /** {@inheritDoc} */
-    @Override
-    public boolean isWaveClean() {
-        return this.aliveEnemiesList.isEmpty() && this.enemyToBeSpawned.isEmpty();
-    }
+	/** {@inheritDoc} */
+	@Override
+	public boolean isWaveClean() {
+		return this.aliveEnemiesList.isEmpty() && this.enemyToBeSpawned.isEmpty();
+	}
 
-    /** {@inheritDoc} */
-    @Override
-    public int getWave() {
-        return this.actualWave;
-    }
+	/** {@inheritDoc} */
+	@Override
+	public int getWave() {
+		return this.actualWave;
+	}
 
-    /** {@inheritDoc} */
-    @Override
-    public void setNextWave() {
-        this.actualWave++;
-        this.enemyToBeSpawned = this.waveFactory.createEnemiesToBeSpawn(actualWave);
-    }
+	/** {@inheritDoc} */
+	@Override
+	public void setNextWave() {
+		this.actualWave++;
+		this.enemyToBeSpawned = this.waveFactory.createEnemiesToBeSpawn(actualWave);
+	}
 
-    /** {@inheritDoc} */
-    @Override
-    public List<Enemy> getEnemies() {
-        return Collections.unmodifiableList(this.aliveEnemiesList);
-    }
+	/** {@inheritDoc} */
+	@Override
+	public List<Enemy> getEnemies() {
+		return Collections.unmodifiableList(this.aliveEnemiesList);
+	}
 
-    /** {@inheritDoc} */
-    @Override
-    public boolean areEnemiesOnBoard() {
-        return !this.aliveEnemiesList.isEmpty();
-    }
+	/** {@inheritDoc} */
+	@Override
+	public boolean areEnemiesOnBoard() {
+		return !this.aliveEnemiesList.isEmpty();
+	}
 
-    /** {@inheritDoc} */
-    @Override
-    public boolean areEnemiesInQueue() {
-        return !this.enemyToBeSpawned.isEmpty();
-    }
+	/** {@inheritDoc} */
+	@Override
+	public boolean areEnemiesInQueue() {
+		return !this.enemyToBeSpawned.isEmpty();
+	}
 
 }
--- src/main/java/tas/controller/MainControllerImpl.java
+++ src/main/java/tas/controller/MainControllerImpl.java
@@ -11,81 +11,81 @@ import main.java.tas.model.GameSpecs;
  */
 public class MainControllerImpl implements MainController {
 
-    private SceneController sceneController;
-    private final MainView mainView;
-    private GameScene scene;
-    private GameSpecs gameSpecs = new GameSpecs();
+	private SceneController sceneController;
+	private final MainView mainView;
+	private GameScene scene;
+	private GameSpecs gameSpecs = new GameSpecs();
 
-    /**
-     * Constructor that creates the main controller of the game.
-     */
-    public MainControllerImpl() {
-        this.mainView = new MainView();
-        this.sceneController = createGame(this.mainView);
+	/**
+	 * Constructor that creates the main controller of the game.
+	 */
+	public MainControllerImpl() {
+		this.mainView = new MainView();
+		this.sceneController = createGame(this.mainView);
 
-        this.mainView.show();
-    }
+		this.mainView.show();
+	}
 
-    /** {@inheritDoc} */
-    @Override
-    public SceneController createMenu(final MainView view) {
-        // TODO
-        return null;
-    }
+	/** {@inheritDoc} */
+	@Override
+	public SceneController createMenu(final MainView view) {
+		// TODO
+		return null;
+	}
 
-    /** {@inheritDoc} */
-    @Override
-    public SceneController createGame(final MainView view) {
-        this.scene = new GameSceneImpl(view.getPanel());
-        SceneController controller = new GameController(this.scene, new GameModelImpl(100, 150));
-        this.scene.setObserver(controller);
-        return controller;
-    }
+	/** {@inheritDoc} */
+	@Override
+	public SceneController createGame(final MainView view) {
+		this.scene = new GameSceneImpl(view.getPanel());
+		SceneController controller = new GameController(this.scene, new GameModelImpl(100, 150));
+		this.scene.setObserver(controller);
+		return controller;
+	}
 
-    /** {@inheritDoc} */
-    @Override
-    public SceneController getController() {
-        return this.sceneController;
-    }
+	/** {@inheritDoc} */
+	@Override
+	public SceneController getController() {
+		return this.sceneController;
+	}
 
-    /** {@inheritDoc} */
-    @Override
-    public void mainLoop() {
-        double next_game_tick = System.currentTimeMillis(); // TODO: qui in mezzo c'e' roba per l'FPS counter, sarebbe
-                                                            // meglio rimuoverli
-        double last_frame_time = System.currentTimeMillis();
-        int loops;
-        int fps = 0;
+	/** {@inheritDoc} */
+	@Override
+	public void mainLoop() {
+		double next_game_tick = System.currentTimeMillis(); // TODO: qui in mezzo c'e' roba per l'FPS counter, sarebbe
+															// meglio rimuoverli
+		double last_frame_time = System.currentTimeMillis();
+		int loops;
+		int fps = 0;
 
-        while (true) { // TODO: cambiare il true con qualcosa di piu' concreto tipo il click di un
-                       // pulsante o altro
-            loops = 0;
+		while (true) { // TODO: cambiare il true con qualcosa di piu' concreto tipo il click di un
+						// pulsante o altro
+			loops = 0;
 
-            while (System.currentTimeMillis() > next_game_tick && loops < this.gameSpecs.getMaxFrameSkip()) {
-                this.sceneController.nextTick();
+			while (System.currentTimeMillis() > next_game_tick && loops < this.gameSpecs.getMaxFrameSkip()) {
+				this.sceneController.nextTick();
 
-                next_game_tick += this.gameSpecs.getSkipTicks();
-                loops++;
-                fps++;
-            }
+				next_game_tick += this.gameSpecs.getSkipTicks();
+				loops++;
+				fps++;
+			}
 
-            if (System.currentTimeMillis() - last_frame_time > 1000) {
-                last_frame_time = System.currentTimeMillis();
-                System.out.println(fps);
-                fps = 0;
-            }
+			if (System.currentTimeMillis() - last_frame_time > 1000) {
+				last_frame_time = System.currentTimeMillis();
+				System.out.println(fps);
+				fps = 0;
+			}
 
-            this.mainView.update();
-        }
-    }
+			this.mainView.update();
+		}
+	}
 
-    /**
-     * The main method that starts the game.
-     * 
-     * @param args not used
-     */
-    public static void main(final String[] args) {
-        new MainControllerImpl().mainLoop();
-    }
+	/**
+	 * The main method that starts the game.
+	 * 
+	 * @param args not used
+	 */
+	public static void main(final String[] args) {
+		new MainControllerImpl().mainLoop();
+	}
 
 }
