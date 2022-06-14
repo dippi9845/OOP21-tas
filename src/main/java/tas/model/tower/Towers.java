8765ce7acfded4952ecba9d4a668a4d077802d7d
Filippo Di Pietro <filippo.dipietro3@studio.unibo.it>
2022-06-14 20:22:47 +0200

    Moved synchronized to getVisibleEnemyList()

--- src/main/java/tas/model/tower/Towers.java
+++ src/main/java/tas/model/tower/Towers.java
@@ -74,10 +74,8 @@ public class Towers {
 	 * @return Optional of enemy, that is empty if no enemy was found, otherwise the
 	 *         enemy found
 	 */
-	static public Optional<Enemy> findFistEnemyByPredicate(final Predicate<Enemy> f, final List<Enemy> enemyList) {
-		synchronized (enemyList) {	
-			return enemyList.stream().filter(f).findFirst();
-		}
+	static public Optional<Enemy> findFistEnemyByPredicate(final Predicate<Enemy> f, final List<Enemy> enemyList) {	
+		return enemyList.stream().filter(f).findFirst();
 	}
 
 	/**
@@ -92,9 +90,7 @@ public class Towers {
 	 */
 	static public Optional<Enemy> findFistEnemyBiPredicate(final Predicate<Enemy> f, final Predicate<Enemy> g,
 			final List<Enemy> enemyList) {
-		synchronized (enemyList) {
-			return enemyList.stream().filter(x->!x.isDead()).filter(f).filter(g).findFirst();
-		}
+		return enemyList.stream().filter(x->!x.isDead()).filter(f).filter(g).findFirst();
 	}
 
 	/**
@@ -105,9 +101,7 @@ public class Towers {
 	 * @return List containing all the enemies that satisfies the predicate
 	 */
 	static public List<Enemy> findAll(final Predicate<Enemy> f, final List<Enemy> enemyList) {
-		synchronized (enemyList) {
-			return enemyList.stream().filter(x->!x.isDead()).filter(f).collect(Collectors.toList());
-		}
+		return enemyList.stream().filter(x->!x.isDead()).filter(f).collect(Collectors.toList());
 	}
 
 	/**
