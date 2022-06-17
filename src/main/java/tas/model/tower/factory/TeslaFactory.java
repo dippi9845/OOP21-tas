ba2e75fce03320cc127c41f2a7caf5c90181eb39
Filippo Di Pietro <filippo.dipietro3@studio.unibo.it>
2022-06-17 19:08:29 +0200

    Added the abstraction to the factory for the mortar and tesla
    i mean the use of getRange of TowerBuilder, so when the builder is getting the info from the json file, i have not to duplicate the info, already present in the json

--- src/main/java/tas/model/tower/factory/TeslaFactory.java
+++ src/main/java/tas/model/tower/factory/TeslaFactory.java
@@ -22,9 +22,9 @@ public interface TeslaFactory {
 	 * @return Tesla tower
 	 */
 	static public Tower basicTesla(final Position pos, final List<Enemy> enemyList) {
-		return new TowerBuilder(pos, DefaultTowersInfo.TOWERSJSONOBJECT.get(DefaultTowers.BASICTESLA), enemyList)
-				.attackType(AttackType.AREA)
-				.damageRange(400)
+		final TowerBuilder t = new TowerBuilder(pos, DefaultTowersInfo.TOWERSJSONOBJECT.get(DefaultTowers.BASICTESLA), enemyList);
+		return t.attackType(AttackType.AREA)
+				.damageRange(t.getRange())
 				.setUpgradable(true)
 				.upgradeCost(x -> 15)
 				.upgradeDamage(x -> 50)
@@ -32,8 +32,9 @@ public interface TeslaFactory {
 				.startUpgradeCost(10)
 				.maximumTarget(4)
 				.findFirst(() -> {
-					return Towers.findFirstEnemyInRange(pos, 500, enemyList);
+					return Towers.findFirstEnemyInRange(pos, t.getRange(), enemyList);
 				}).build();
+		
 	}
 
 	/**
@@ -44,9 +45,9 @@ public interface TeslaFactory {
 	 * @return Super Tesla tower
 	 */
 	static public Tower superTesla(final Position pos, final List<Enemy> enemyList) {
-		return new TowerBuilder(pos, DefaultTowersInfo.TOWERSJSONOBJECT.get(DefaultTowers.SUPERTESLA), enemyList)
-				.attackType(AttackType.AREA)
-				.damageRange(400)
+		final TowerBuilder t = new TowerBuilder(pos, DefaultTowersInfo.TOWERSJSONOBJECT.get(DefaultTowers.SUPERTESLA), enemyList);
+		return t.attackType(AttackType.AREA)
+				.damageRange(t.getRange())
 				.setUpgradable(true)
 				.upgradeCost(x -> x * x)
 				.upgradeDamage(x -> x * x * 3)
@@ -54,7 +55,7 @@ public interface TeslaFactory {
 				.startUpgradeCost(20)
 				.maximumTarget(8)
 				.findFirst(() -> {
-					return Towers.findFirstEnemyInRange(pos, 500, enemyList);
+					return Towers.findFirstEnemyInRange(pos, t.getRange(), enemyList);
 				}).build();
 	}
 
@@ -66,9 +67,9 @@ public interface TeslaFactory {
 	 * @return God Tesla tower
 	 */
 	static public Tower godTesla(final Position pos, final List<Enemy> enemyList) {
-		return new TowerBuilder(pos, DefaultTowersInfo.TOWERSJSONOBJECT.get(DefaultTowers.GODTESLA), enemyList)
-				.attackType(AttackType.AREA)
-				.damageRange(400)
+		final TowerBuilder t = new TowerBuilder(pos, DefaultTowersInfo.TOWERSJSONOBJECT.get(DefaultTowers.GODTESLA), enemyList);
+		return t.attackType(AttackType.AREA)
+				.damageRange(t.getRange())
 				.setUpgradable(true)
 				.upgradeCost(x -> (int) Math.pow(x, x) + 20)
 				.upgradeDamage(x -> (int) Math.pow(x, x))
@@ -76,7 +77,7 @@ public interface TeslaFactory {
 				.startUpgradeCost(100)
 				.maximumTarget(100)
 				.findFirst(() -> {
-					return Towers.findFirstEnemyInRange(pos, 500, enemyList);
+					return Towers.findFirstEnemyInRange(pos, t.getRange(), enemyList);
 				}).build();
 	}
 }
