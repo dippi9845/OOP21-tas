4b4f18da961452b322b91d1a2c6dd75b4512fb4c
Filippo Di Pietro <filippo.dipietro3@studio.unibo.it>
2022-06-21 00:57:14 +0200

    Added javadoc to TowerThreadImpl

--- src/main/java/tas/controller/tower/TowerThreadImpl.java
+++ src/main/java/tas/controller/tower/TowerThreadImpl.java
@@ -3,15 +3,24 @@ package main.java.tas.controller.tower;
 import main.java.tas.model.tower.Tower;
 import main.java.tas.utils.Position;
 
+/**
+ * A class that model a thread that contains a tower, and it run concurrently to the main thread
+ *
+ */
 public class TowerThreadImpl implements TowerThread{
 	private final Tower tower;
 	private boolean stopTh;
 	
+	/**
+	 * Constructor, with only one parameter, a tower that we want to run in our thread
+	 * @param t tower that we want to run in our thread
+	 */
 	protected TowerThreadImpl(final Tower t) {
 		this.tower = t;
 		this.stopTh = false;
 	}
 	
+	/** {@inheritDoc} */
 	@Override
 	public void run() {
 		while (!this.isStop()) {
@@ -20,46 +29,55 @@ public class TowerThreadImpl implements TowerThread{
 
 	}
 
+	/** {@inheritDoc} */
 	@Override
 	public boolean isStop() {
 		return this.stopTh;
 	}
 
+	/** {@inheritDoc} */
 	@Override
 	public void stop() {
 		this.stopTh = true;
 	}
 
+	/** {@inheritDoc} */
 	@Override
 	public void compute() {
 		this.tower.compute();
 	}
 
+	/** {@inheritDoc} */
 	@Override
 	public int getDamage() {
 		return this.tower.getDamage();
 	}
 
+	/** {@inheritDoc} */
 	@Override
 	public int getRadius() {
 		return this.tower.getRadius();
 	}
 
+	/** {@inheritDoc} */
 	@Override
 	public int getCost() {
 		return this.tower.getCost();
 	}
 
+	/** {@inheritDoc} */
 	@Override
 	public int getDelay() {
 		return this.tower.getDelay();
 	}
 
+	/** {@inheritDoc} */
 	@Override
 	public String getTowerName() {
 		return this.tower.getTowerName();
 	}
 
+	/** {@inheritDoc} */
 	@Override
 	public Position getPos() {
 		return this.tower.getPos();
